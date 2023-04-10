8b6dbbb9e647c6e60e3d8f773839649a24e9ec28
Adam La Rosa <adam.larosa@flatironschool.com>
2023-04-10 08:51:12 -0700

    serialized

--- server/models.py
+++ server/models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 class Doctor( db.Model, SerializerMixin ):
     __tablename__ = 'doctors'
+    serialize_rules = ( '-appointments.doctor', )
     id = db.Column( db.Integer, primary_key = True )
     name = db.Column( db.String )
     department = db.Column( db.String )
@@ -13,12 +14,14 @@ class Doctor( db.Model, SerializerMixin ):
 
 class Appointment( db.Model, SerializerMixin ):
     __tablename__ = 'appointments'
+    serialize_rules = ( '-patient.appointments', '-doctor.appointments' )
     id = db.Column( db.Integer, primary_key = True )
     note = db.Column( db.String )
     doctor_id = db.Column( db.Integer, db.ForeignKey( 'doctors.id' ) )
     patient_id = db.Column( db.Integer, db.ForeignKey( 'patients.id' ) )
 
 class Patient( db.Model, SerializerMixin ):
+    serialize_rules = ( '-appointments.patient', )
     __tablename__ = 'patients'
     id = db.Column( db.Integer, primary_key = True )
     name = db.Column( db.String )
